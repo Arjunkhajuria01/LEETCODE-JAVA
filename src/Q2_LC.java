877724dd33f5ca2d4843422a927a3d5f17ef6096
Arjunkhajuria01 <bharatsaida67@gmail.com>
2025-09-14 21:14:33 +0530

    update

--- src/Q2_LC.java
+++ src/Q2_LC.java
@@ -2,20 +2,19 @@ public class Q2_LC {
     public static void main(String[] args) {
         class Solution {
             public int removeElement(int[] nums, int val) {
-                int k = 0;
-
-                for ( int i = 0 ; i<= nums.length -1 ; i++){
+                int index = 0;
+                for( int i = 0; i<= nums.length-1 ; i++){
                     if (nums[i] == val){
 
                     }
-                    else if (nums[i] != val){
-                        nums[k] = nums[i];
-                        k++;
+                    else if(nums[i] != val){
+                        nums[index] = nums[i];
+                        index++;
                     }
-                }
-                return k;
-            }
 
+                }
+                return index;
+            }
         }
     }
 }
