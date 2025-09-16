2fed82007d10060347d25ded06b51e5103a8025c
Arjunkhajuria01 <bharatsaida67@gmail.com>
2025-09-16 21:52:09 +0530

    update

---
+++ src/Q6_LC.java
@@ -0,0 +1,41 @@
+public class Q6_LC {
+    public static void main(String[] args) {
+        class Solution {
+            public String[] findRelativeRanks(int[] score) {
+                //      String[]Medel = new String[score.length];
+
+                //     for(int i = 0; i<= score.length -1 ; i++){
+                //         for(int j = i+1 ; j<= score.length -1 ; j++){
+                //             int temp = score[i];
+                //             if(score[j] > score[i]){
+                //             score[i] = score[j];
+                //             }
+                //         }
+
+                // }
+                // for(int i = 0; i<= Medel.length -1 ; i++){
+                //     if(i == 0){
+                //         Medel[i] = "Gold Medal";
+                //     }
+                //     if(i== 1){
+                //         Medel[i] = "Silver Medal";
+                //     }
+                //     if(i== 2){
+                //         Medel[i] = "Bronze Medal";
+                //     }
+                //     else if( i >= 3){
+                //         Medel[i] = "" + (i+1);
+                // }
+
+
+                // }
+                // return Medel;
+
+                int a = 0;
+                for(int i = 0; i<= score.length -1 ; i++){
+
+                }
+            }
+        }
+    }
+}
