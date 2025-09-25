fda3368512c44cbd25fe4fcaae52ea5a0b6bba51
Arjunkhajuria01 <bharatsaida67@gmail.com>
2025-09-25 20:42:12 +0530

    update

---
+++ src/Q9_LTC.java
@@ -0,0 +1,19 @@
+public class Q9_LTC {
+    class Solution {
+        public int majorityElement(int[] nums) {
+
+            //   int b = 0;
+            //   for(int i = 0; i<= nums.length -1 ; i++){
+            //      int a = 0;
+            //     for(int j = 0 ; j<= nums.length -1 ; j++){
+            //         if(nums[i] == nums[j]){
+            //             a++;
+            //         }
+            //     }
+            //     if(a> nums.length /2){
+            //         b = nums[i];
+            //     }
+            //   }
+            //   return b;
+
+        }
